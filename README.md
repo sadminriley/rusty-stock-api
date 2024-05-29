28c297c3b0c5a3b18ca5d732593f66bae36888a9
Riley Louvier <rileylouvier@MacBook-Pro.local>
2024-05-29 03:10:46 -0600

    Add ability to use clap arg parser

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Rust Stock API
 A stock api written in rust using Financial Modeling Prep
+
+
+
+Example using clap -
+
+```
+$ cargo run TSLA
+
+     Running `target/debug/financial-modeling-prep-api TSLA`
+Stock quote for TSLA: [
+  {
+    "symbol": "TSLA",
+    "price": 176.75,
+    "volume": 59394590
+  }
+]
+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 extern crate reqwest;
 extern crate tokio;
 extern crate dotenv;
+extern crate clap;
 
 use reqwest::header::{HeaderMap, UPGRADE_INSECURE_REQUESTS};
 use dotenv::dotenv;
 use std::env;
 use tokio::main;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// CLI args to pass to clap
+    symbol: String,
+}
 
 async fn get_stock_quote(symbol: &str) -> Result<String, reqwest::Error> {
     dotenv().ok(); // Load environment variables from .env file
@@ -33,9 +41,10 @@ async fn get_stock_quote(symbol: &str) -> Result<String, reqwest::Error> {
 
 #[main]
 async fn main() -> Result<(), reqwest::Error> {
-    let symbol: &str = "TSLA"; // Pass any stock symbol
-    match get_stock_quote(symbol).await {
-        Ok(quote) => println!("Stock quote for {}: {}", symbol, quote),
+    let args = Cli::parse();
+
+    match get_stock_quote(&args.symbol).await {
+        Ok(quote) => println!("Stock quote for {}: {}", args.symbol, quote),
         Err(e) => eprintln!("Error fetching stock quote: {}", e),
     }
 
